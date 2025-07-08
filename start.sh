cd310042e6632f43c9627026af33ac0932243238
crammaster <50364743+crammaster@users.noreply.github.com>
2025-07-08 22:21:43 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,11 @@
-!#
+#!/bin/sh
+
+# Prüfen, ob das Script als root läuft
+if [ "$(id -u)" -ne 0 ]; then
+  echo "Dieses Script benötigt Root-Rechte. Starte das Script bitte mit sudo..."
+  exec sudo "$0" "$@"
+fi
+
+# Ab hier kommen die Befehle
+apt update
+apt upgrade -y
